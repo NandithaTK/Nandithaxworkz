703074be7f9196381f73c461d9436c1e6b398de8
Nandithaxworkz <nandithatk91.xworkz@gmail.com>
2020-12-24 05:42:52 +0530

    adding restaurant files

---
+++ restaurant-proj/src/com/xworkz/restaurant/Restaurant.java
@@ -0,0 +1,26 @@
+package com.xworkz.restaurant;
+
+public class Restaurant {
+public static int id;
+//instance block or non-static block or init block or IIB (instance initialisation block)
+{
+	id = 2345;
+	System.out.println(id);
+}
+//static block or class block or SIB (Static Initialisation block)
+static {
+	
+}
+public void serveFood()
+{
+	System.out.println();
+}
+
+
+
+
+
+}
+
+
+
---
+++ restaurant-proj/src/com/xworkz/restutil/RestUtil.java
@@ -0,0 +1,12 @@
+package com.xworkz.restutil;
+
+public class RestUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
+
