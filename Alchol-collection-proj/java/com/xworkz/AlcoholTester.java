603cd1feb0529ab386c72b61d7e66ee149afdb05
NandithaTK <75018989+NandithaTK@users.noreply.github.com>
2021-02-14 23:00:07 +0530

    Adding Collections List example

---
+++ Alchol-collection-proj/java/com/xworkz/AlcoholTester.java
@@ -0,0 +1,47 @@
+package com.xworkz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xworkz.DTO.AlcoholDTO;
+
+public class AlcoholTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AlcoholDTO alcoholDTO1 = new AlcoholDTO("IB", "Imperial", 48);
+
+		AlcoholDTO alcoholDTO2 = new AlcoholDTO("Amstel", "King Fisher", 4);
+
+		AlcoholDTO alcoholDTO3 = new AlcoholDTO("Magic Moments", "NA", 38);
+
+		AlcoholDTO alcoholDTO4 = new AlcoholDTO("Old Monk", "NA", 42);
+
+		List<AlcoholDTO> alcoholDTOs = new ArrayList<AlcoholDTO>(100);
+		alcoholDTOs.add(alcoholDTO4);
+		alcoholDTOs.add(1, alcoholDTO2);
+		alcoholDTOs.add(alcoholDTO3);//magic moments
+		alcoholDTOs.add(alcoholDTO1);//IB
+
+		 //alcoholDTOs.remove(alcoholDTO3); // it invokes equals method inturn internally.
+		//alcoholDTOs.remove(2);
+		
+		AlcoholDTO alcoholDTO5 = new AlcoholDTO("Bud Wiser", "King Fisher", 8);
+
+		alcoholDTOs.add(4, alcoholDTO5);
+		
+		
+		int index=alcoholDTOs.indexOf(alcoholDTO5);
+		System.out.println("indexOf"+index);
+		
+		int lastIndex=alcoholDTOs.lastIndexOf(alcoholDTO3);
+		System.out.println("lastIndex"+lastIndex);
+		for (AlcoholDTO alcoholDTO : alcoholDTOs) {
+			System.out.println(alcoholDTO);
+		}
+	}
+
+	}
+
+
